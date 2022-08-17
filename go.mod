c90d0ac03c60bc75cde00c631e4de428aa071e7a
Reza Juliandri <rezajuliandri20@gmail.com>
2022-08-17 20:54:02 +0700

    [Init] Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ppabcd/app-say-hello
+
+go 1.19
+
+require github.com/ppabcd/go-say-hello/v2 v2.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	gosayhello "github.com/ppabcd/go-say-hello/v2"
+)
+
+func main() {
+	fmt.Println(gosayhello.SayHello("Reza"))
+}
